f3c3629e402eeeff512a709e8244419ffc606546
Boushaki01 <kikiforwork13@gmail.com>
2022-10-01 23:28:56 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(NavAR());
+  runApp(FindRoom());
 }
 
-class NavAR extends StatelessWidget {
-  const NavAR({Key? key}) : super(key: key);
+class FindRoom extends StatelessWidget {
+  const FindRoom({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text("On Progress..."),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Image(
+            image: AssetImage("lib/assets/Logo.png"),
+            width: 170,
+            height: 170,
+          ),
+        ),
       ),
-    ));
+    );
   }
 }

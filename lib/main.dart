6f3813ceaa73f1e6377a377172de7d4b50058880
AzandyMuhammad <114732334+AzandyMuhammad@users.noreply.github.com>
2022-10-15 11:01:54 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:navigationar/application.dart';
 
 void main() {
-  runApp(FindRoom());
+  runApp(const Application());
 }
 
-class FindRoom extends StatelessWidget {
-  const FindRoom({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Image(
-            image: AssetImage("lib/assets/Logo.png"),
-            width: 170,
-            height: 170,
-          ),
-        ),
-      ),
-    );
-  }
-}

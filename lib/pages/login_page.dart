b18278068067e68ebaf3eb4027d06d59d04840b9
AzandyMuhammad <114732334+AzandyMuhammad@users.noreply.github.com>
2022-10-15 11:13:51 +0700

    Create login_page.dart

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:navigationar/style/style.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 200,
+            ),
+            Image.asset('assets/logo.png'),
+            const SizedBox(
+              height: 60,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 36,
+              ),
+              margin: const EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 30,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelStyle: const TextStyle(
+                        color: greyColor,
+                      ),
+                      fillColor: orangeSecondary,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      labelText: 'NIP / NPM',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelStyle: const TextStyle(
+                        color: greyColor,
+                      ),
+                      fillColor: orangeSecondary,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      labelText: 'Password',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      backgroundColor: orangePrimary,
+                    ),
+                    child: const Text('Login'),
+                    onPressed: () {},
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      backgroundColor: orangePrimary,
+                    ),
+                    child: const Text('Login Single On USK'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

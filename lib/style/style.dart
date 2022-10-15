09bdfe6ad48d9d78ade1520c14e8253259a7de6f
AzandyMuhammad <114732334+AzandyMuhammad@users.noreply.github.com>
2022-10-15 11:16:59 +0700

    Create style.dart

---
+++ lib/style/style.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color orangePrimary = Color(0xffFF9F46);
+const Color greyColor = Color(0xff8CA2B2);
+const Color orangeSecondary = Color(0xffFFBC81);
